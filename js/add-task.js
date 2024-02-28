064ab28f1b3a167f0b854f5ab23de0b57ad3cfcb
AMAUXRIV <maulanapratam41@gmail.com>
2024-02-28 07:08:53 +0700

    0.43

--- js/add-task.js
+++ js/add-task.js
@@ -2,6 +2,12 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2,"0");
+    const day = String(now.getDate() + 1).padStart(2,"0");
+
+
     const taskForm = document.getElementById('taskForm');
     const taskManager = new Task();
 
@@ -12,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskData = {
             taskName:document.getElementById('taskName').value,
             taskPriority:document.getElementById('taskPriority').value,
+            createdAt:`${year}-${month}-${day}`
         };
 
         const result = taskManager.saveTask(taskData);
